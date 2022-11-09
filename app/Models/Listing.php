ab8d083f441a02fcd1d6118d6e19d556bb57d333
David Amebley <davameb@gmail.com>
2022-11-09 21:09:55 +0200

    Set up listing tag filter through a Scope filter in the Listing Model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    // Using one of Laravel's Eloquent Functions
+    public function scopeFilter($query, array $filter)
+    {
+        if ($filter['tag'] ?? false) {    //If this does not return false, then..
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+    }
 }
