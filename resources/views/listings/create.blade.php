6375b90bbd39ad9af4878046bb67726b19a08035
David Amebley <davameb@gmail.com>
2022-11-10 23:19:19 +0200

    Add listing logo fields to Listings table migration

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -85,8 +85,12 @@
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
-                {{-- <input type="file" class="border border-gray-200 rounded p-2 w-full"
-                    name="logo"/> --}}
+                <input type="file" class="border border-gray-200 rounded p-2 w-full"
+                    name="logo"/>
+
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="mb-6">
