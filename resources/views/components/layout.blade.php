9f09b7c22251c3f84c130ef8824bbe5d64a2c8b5
David Amebley <davameb@gmail.com>
2022-11-12 00:25:14 +0200

    Display logout button

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -41,6 +41,14 @@
                         ><i class="fa-solid fa-gear"></i>
                         Manage Listings</a>
                 </li>
+                <li>
+                    <form action="/logout" method="POST" class="inline">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                        </button>
+                    </form>
+                </li>
                 @else
                 <li>
                     <a href="/register" class="hover:text-laravel"
